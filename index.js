ab3b0da6a5d916b079131f119719faa04896dd6c
sajjadmolliek <sajjadmolliek2018@gmail.com>
2023-10-19 02:15:30 +0600

    Insertion Operation Successfull

--- index.js
+++ index.js
@@ -9,6 +9,44 @@ const port = process.env.PORT || 5005;
 app.use(cors());
 app.use(express.json());
 
+// MongoDB COde:
+
+const uri = `mongodb+srv://${process.env.DB_U_NAME}:${process.env.DB_PASS}@cluster0.rsgizg7.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const AddProductCollection = client.db("AddProductDB").collection("Products");
+
+    // --------------------------------AddProductCollection Data Collection Server--------------------------------------
+
+    // Insert Into Database:
+    app.post("/addProduct", async (req, res) => {
+      const addingProducts = req.body;
+      console.log(addingProducts);
+      const result = await AddProductCollection.insertOne(addingProducts);
+      res.send(result);
+    });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello I am REST API");

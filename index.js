80830c801921556b2ad56bb5091d7b1381eb3cf2
sajjadmolliek <sajjadmolliek2018@gmail.com>
2023-10-19 07:07:52 +0600

    DE Operation Successfull

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
+
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
@@ -34,10 +35,51 @@ async function run() {
     // Insert Into Database:
     app.post("/addProduct", async (req, res) => {
       const addingProducts = req.body;
-      console.log(addingProducts);
       const result = await AddProductCollection.insertOne(addingProducts);
       res.send(result);
     });
+    // Read From Database:
+    app.get("/addProduct", async (req, res) => {
+      const cursor = AddProductCollection.find();
+      const result = await cursor.toArray();
+
+      res.send(result);
+    });
+
+    // POST and Read From Database:
+    app.post("/addProducts", async (req, res) => {
+      try {
+        const products = req.body; // This contains the brand_name sent from the frontend
+    
+        // Use the brand_name to query your MongoDB collection
+        const query = { brand_name: products.brand_name};
+        const cursor = AddProductCollection.find(query);
+        const result = await cursor.toArray();
+    
+        if (result.length > 0) {
+          // Products matching the brand_name were found
+          res.send(result);
+        } else {
+          // No products found for the specified brand_name
+          res.send([]);
+        }
+      } catch (error) {
+        console.error("Error querying the database:", error);
+        res.status(500).send("Internal Server Error");
+      }
+    });
+
+    
+    // Delete Data From Database
+    app.delete("/addProducting/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await AddProductCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
